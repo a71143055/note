280e0dbff6a023c1ab73769e526d67690a3a3eee
a71143055 <a71143055@gmail.com>
2025-11-23 20:55:20 +0900

    note

--- src/main/java/com/example/note/config/SecurityConfig.java
+++ src/main/java/com/example/note/config/SecurityConfig.java
@@ -5,9 +5,7 @@ import com.example.note.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -53,7 +51,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/signup")) // 데모 편의상
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/signup", "/css/**").permitAll()
                         .requestMatchers("/", "/notes/**").authenticated()
@@ -71,8 +68,7 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/login?logout")
                         .deleteCookies("JSESSIONID")
                         .permitAll()
-                )
-                .httpBasic(Customizer.withDefaults()); // 개발 편의
+                );
 
         return http.build();
     }
